964a790dd66c32e177dd38541c96cc43f97fede7
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2025-03-13 20:15:59 +0000

    implement coffee service endpoint

--- coffee-service/src/app.controller.ts
+++ coffee-service/src/app.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get(':coffeeName/:waterName')
+  async getBeverage(
+    @Param('coffeeName') coffeeName: string,
+    @Param('waterName') waterName: string,
+  ) {
+    return this.appService.getBeverageDetails(coffeeName, waterName);
   }
 }
--- coffee-service/src/app.service.ts
+++ coffee-service/src/app.service.ts
@@ -1,8 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { Injectable, NotFoundException } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly httpService: HttpService) {}
+
+  private async fetchCoffee(name: string) {
+    try {
+      const response = await this.httpService.axiosRef.get(
+        `http://localhost:3011/coffee-type/${name}`,
+      );
+      return response.data;
+    } catch (error) {
+      throw new NotFoundException(`Coffee with name "${name}" not found`);
+    }
+  }
+
+  private async fetchWater(name: string) {
+    try {
+      const response = await this.httpService.axiosRef.get(
+        `http://localhost:3010/water/${name}`,
+      );
+      return response.data;
+    } catch (error) {
+      throw new NotFoundException(`Water with name "${name}" not found`);
+    }
+  }
+
+  async getBeverageDetails(coffeeName: string, waterName: string) {
+    const [coffee, water] = await Promise.all([
+      this.fetchCoffee(coffeeName),
+      this.fetchWater(waterName),
+    ]);
+
+    return {
+      coffee,
+      water,
+    };
   }
 }
